ca40dbfab9607fd5402f4affa88502f5f18037ff
Maxthod <maxthod@pm.me>
2022-02-22 00:38:31 -0500

    Continue...

--- src/main/java/com/duvalhub/dummy/Controller.java
+++ src/main/java/com/duvalhub/dummy/Controller.java
@@ -1,6 +1,8 @@
 package com.duvalhub.dummy;
 
+import com.duvalhub.core.exceptions.DuvalhubException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,10 @@ public class Controller {
     @GetMapping
     public String allo() {
         return "Allo from dummy";
+    }
+
+    @PostMapping
+    public String error() {
+        throw new DuvalhubException();
     }
 }
